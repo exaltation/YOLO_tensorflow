6de47df84f67936b1278fa1a88102c308e6838c1
exaltation <jorillo311@gmail.com>
2017-09-27 10:45:42 +0500

    +

---
+++ testIt.py
@@ -0,0 +1,12 @@
+import sys
+import YOLO_tiny_tf
+
+yolo = YOLO_tiny_tf.YOLO_TF()
+
+yolo.disp_console = False
+yolo.imshow = True
+
+if len(sys.argv) > 1:
+    yolo.tofile_img = sys.argv[1] + '.processed.jpg'
+    yolo.filewrite_img = True
+    yolo.detect_from_file(sys.argv[1])
