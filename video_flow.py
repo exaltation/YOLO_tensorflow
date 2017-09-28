15ea77a66213149b6d67a73f9f4bf63ade32be3f
Jorillo Abdullaev <jorillo311@gmail.com>
2017-09-29 00:19:45 +0500

    +

--- video_flow.py
+++ video_flow.py
@@ -4,16 +4,16 @@ import sys
 import YOLO_small_tf
 
 yolo = YOLO_small_tf.YOLO_TF()
-
+fps = 30.0
 cap = cv2.VideoCapture(sys.argv[1])
 vw = int(cap.get(3))
 vh = int(cap.get(4))
 # for i in range(10): #
 #     print(i, cap.get(i))
-outv = cv2.VideoWriter(sys.argv[1] + '.processed.mp4', cv2.VideoWriter_fourcc(*'mjpg'), 20.0, (vw, vh))
+outv = cv2.VideoWriter(sys.argv[1] + '.processed.mp4', cv2.VideoWriter_fourcc(*'mjpg'), fps, (vw, vh))
 
-inLine = vh//8
-outLine = 2*vh//8
+inLine = 7*vh//8
+outLine = 5*vh//8
 
 while(cap.isOpened()):
     ret, frame = cap.read()
@@ -31,13 +31,13 @@ while(cap.isOpened()):
             cv2.rectangle(frame,(x-w,y-h),(x+w,y+h),(0,255,0),2)
             print(res)
 
-            cv2.line(frame, (0,inLine), (vw,inLine), (255,0,0), 4)
-            cv2.line(frame, (0,outLine), (vw,outLine), (0,255,0), 4)
+    cv2.line(frame, (0,inLine), (vw,inLine), (255,0,0), 2)
+    cv2.line(frame, (0,outLine), (vw,outLine), (0,255,0), 2)
 
     outv.write(frame)
 
     cv2.imshow('Frame',frame)
-    k = cv2.waitKey(30) & 0xff
+    k = cv2.waitKey(1000.0 / fps) & 0xff
     if k == 27:
         break
 
