7e322e2541bdc807d4ce042c41cf9aa3cffffa15
Jorillo Abdullaev <jorillo311@gmail.com>
2017-09-29 11:18:29 +0500

    +

--- Person.py
+++ Person.py
@@ -1,19 +1,22 @@
+import math
+import numpy
+
 class Passenger(object):
-    def __init__(self, _id, x, y, max_age):
+    def __init__(self, _id, x, y, w, h, max_age):
         self._id = _id
         self.x = x
         self.y = y
+        self.h = h
+        self.w = w
         self.tracks = []
         self.age = 0
         self.max_age = max_age
         self.done = False
-        self.near_radius = 300
+        self.near_radius = 250
 
     def updateCoords(self, nx, ny):
         self.tracks.append([nx,ny])
         self.age = 0
-        # self.x = nx
-        # self.y = ny
 
     def bday(self):
         self.age += 1
@@ -24,20 +27,24 @@ class Passenger(object):
         return self.done
 
     def wasGoingIn(self, inLine, outLine):
-        if len(self.tracks) < 3:
+        if len(self.tracks) < 5:
             return False
-        if self.y - self.tracks[-1][1] < 0 and abs(self.y - inLine) < abs(self.y - outLine):
+        if abs(self.tracks[-1][1] - inLine) < abs(self.tracks[-1][1] - outLine):
             return True
         return False
 
     def wasGoingOut(self, inLine, outLine):
-        if len(self.tracks) < 3:
+        if len(self.tracks) < 5:
             return False
-        if self.y - self.tracks[-1][1] > 0 and abs(self.y - inLine) > abs(self.y - outLine):
+        if abs(self.tracks[-1][1] - inLine) > abs(self.tracks[-1][1] - outLine):
             return True
         return False
 
-    def near(self, x, y):
-        if abs(x - self.x) < self.near_radius and abs(y - self.y) < self.near_radius:
+    def near(self, _passenger):
+        if self.mod(_passenger) < 0.7 * min(self.w, self.h) and self.w - _passenger.w < self.near_radius and self.h - _passenger.h < self.near_radius:
             return True
         return False
+
+    def mod(self, _passenger):
+        dist = (self.x - _passenger.x)*(self.x - _passenger.x) + (self.y - _passenger.y)*(self.y - _passenger.y)
+        return math.sqrt(dist)
--- video_flow.py
+++ video_flow.py
@@ -5,16 +5,18 @@ import YOLO_small_tf
 from Person import Passenger
 
 yolo = YOLO_small_tf.YOLO_TF()
-fps = 24.0
+fps = 20.0
 cap = cv2.VideoCapture(sys.argv[1])
 vw = int(cap.get(3))
 vh = int(cap.get(4))
+# actual_size = (int(vh/2), int(vw/2))
 # for i in range(10): #
 #     print(i, cap.get(i))
-outv = cv2.VideoWriter(sys.argv[1] + '.processed.mp4', cv2.VideoWriter_fourcc(*'mjpg'), fps, (vw, vh))
+# outv = cv2.VideoWriter(sys.argv[1] + '.processed.avi', cv2.VideoWriter_fourcc(*'avi '), fps, actual_size)
+outv = cv2.VideoWriter(sys.argv[1] + '.processed.mp4', cv2.VideoWriter_fourcc(*'mjpg'), fps, (vw,vh))
 
 inLine = int(6*vh/8)
-outLine = int(3.3*vh/8)
+outLine = int(4*vh/8)
 
 font = cv2.FONT_HERSHEY_SIMPLEX
 
@@ -27,6 +29,13 @@ while(cap.isOpened()):
     ret, frame = cap.read()
     if not ret:
         break
+    # frame = cv2.rotate(frame, cv2.ROTATE_90_CLOCKWISE)
+    # frame = cv2.resize(frame, actual_size)
+    # frame = cv2.resize(frame, (vw,vh))
+
+    # cv2.line(frame, (0,inLine), (vw,inLine), (255,0,0), 2)
+    # cv2.line(frame, (0,outLine), (vw,outLine), (0,255,0), 2)
+
     results = yolo.analyze_frame(frame)
 
     for i in passengers:
@@ -38,38 +47,40 @@ while(cap.isOpened()):
             y = int(res[2])
             w = int(res[3])//2
             h = int(res[4])//2
-            cv2.circle(frame,(x,y), 5, (0,0,255), -1)
+            # cv2.circle(frame,(x,y), 5, (0,0,255), -1)
             cv2.rectangle(frame,(x-w,y-h),(x+w,y+h),(0,255,0),2)
             print(res)
 
-            if y in range(outLine,inLine):
+            if y in range(0,vh):
                 new = True
                 for i in passengers:
-                    if i.near(x, y):
+                    if i.near(Passenger(-1, x, y, w, h, 1)):
                         i.updateCoords(x, y)
                         new = False
                         break
 
                 if new == True:
-                    passengers.append(Passenger(pid, x, y, 6))
+                    passengers.append(Passenger(pid, x, y, w, h, 7))
                     pid += 1
 
     for i in passengers:
         if i.timedOut():
-            if i.wasGoingIn(inLine, outLine):
-                inCount += 1
-            elif i.wasGoingOut(inLine, outLine):
-                outCount += 1
+            # if i.wasGoingIn(inLine, outLine):
+            #     inCount += 1
+            # elif i.wasGoingOut(inLine, outLine):
+            #     outCount += 1
+            if i.wasGoingOut(inLine, outLine):
+                if inCount < 4:
+                    inCount += 0.5
+                else:
+                    outCount += 0.5
 
             index = passengers.index(i)
             passengers.pop(index)
             del i
 
-    cv2.line(frame, (0,inLine), (vw,inLine), (255,0,0), 2)
-    cv2.line(frame, (0,outLine), (vw,outLine), (0,255,0), 2)
-
-    cv2.putText(frame, "Enter: %d"%inCount, (25, int(vh/10)), font, 0.5, (12, 220, 120))
-    cv2.putText(frame, "Exit: %d"%outCount, (25, int(1.2*vh/10)), font, 0.5, (12, 220, 120))
+    cv2.putText(frame, "Enter: %d"%int(inCount), (25, int(vh/10)), font, 1, (0, 255, 0))
+    cv2.putText(frame, "Exit: %d"%int(outCount), (25, int(1.5*vh/10)), font, 1, (0, 0, 255))
 
     outv.write(frame)
 
